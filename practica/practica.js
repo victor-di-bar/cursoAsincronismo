0b7ab1d33a3a17d93f37f9cfc17b60545c385d9b
victor-di-bar <victor_damm_8@hotmail.com>
2022-08-01 18:55:52 +0200

    Practica de los conceptos

--- practica/practica.js
+++ practica/practica.js
@@ -18,20 +18,174 @@
 
 //callbacks asincronos: se ejecuta solamente cuando algo pasa, no son bloqueadoras
 
-const bajaVideo = (url, callback) => {
-    console.log(`Bajando video de ${url}`);
-    setTimeout(() => {
-        console.log(`Video de ${url} descargado`);
-        callback(url)
-    }, 2000);
-};
+// const bajaVideo = (url, callback) => {
+//     console.log(`Bajando video de ${url}`);
+//     setTimeout(() => {
+//         console.log(`Video de ${url} descargado`);
+//         callback(url)
+//     }, 2000);
+// };
 
-const procesaVideo = (url) => {
-    console.log(`Procesando video de ${url}`);
-};
+// const procesaVideo = (url) => {
+//     console.log(`Procesando video de ${url}`);
+// };
 
-let url = `google.com` 
+// let url = `google.com` 
 
-bajaVideo(url, procesaVideo);
+// bajaVideo(url, procesaVideo);
 // procesaVideo(url);
 
+//promises son objetos que representan el resultado eventual de una operacion asincrona
+//puede tener 3 estados:
+// pending (pendiente de cumplirse o no), resolve (se cumplió) y reject (no se cumplió)
+
+// let teHasPortadoBien = false;
+
+// const siMePortoBien = new Promise ((resolve, reject) => {
+//     if (teHasPortadoBien) {
+//         const telefono = {
+//             modelo: `Iphone 12`,
+//             capacidad: `128Gb`,
+//             color: `rojo`
+//         }
+//         resolve(telefono);
+//     } else {
+//         reject(`Te has portado mal`);
+//     }
+// });
+
+// const promesaCumplida = (resolvedValue) => {
+//     console.log(`Te regalo tu nuevo ${resolvedValue.modelo}`);
+// };
+
+// const promesaRota = (rejectedValue) => {
+//     console.log(`No te compro nada porque ${rejectedValue}`);
+// };
+
+// // siMePortoBien.then(promesaCumplida, promesaRota); //poco intuitiva
+
+// const pideRegalo = () => {
+//     siMePortoBien
+//         .then(promesaCumplida)
+//         .catch(promesaRota)
+// };
+
+// pideRegalo();
+
+//chaining promises
+
+// let comportamiento = true;
+// let teHasPortadoBien = false;
+
+// const chequeaComportamiento = (comportamiento) => {
+//     return new Promise ((resolve, reject) => {
+        // if (teHasPortadoBien) {
+        //     const telefono = {
+        //         modelo: `Iphone 12`,
+        //         capacidad: `128Gb`,
+        //         color: `rojo`
+        //     }
+        //     resolve(telefono);
+        // } else {
+        //     reject(`Te has portado mal`);
+//         }
+//     });
+// };
+
+// const grabaVideo = (telefono) => {
+//     const mensaje = `Grabo video para tiktok con mi ${telefono.modelo}`;
+//     return Promise.resolve(mensaje);
+// };
+
+
+// chequeaComportamiento(comportamiento)
+//     .then((resolvedValue) => {
+//         return grabaVideo(resolvedValue);
+//     })
+//     .then((resolvedValue) => {
+//         console.log(resolvedValue);
+//     })
+//     .catch((errorMessage) => { 
+//         console.log(errorMessage)
+//     })
+
+//promise all --> para cuando te da igual el orden. Toma como argumento un array de promesas y retorna una sola promesa
+// si hay una promesa que no se cumple, Promise.all rechaza con la razón específica
+//si todas las promesas se cumplen, Promise.all se resuelve con un array que contiene los valores especificados en resolve de cada promesa
+
+// const chequeaComportamiento = (comportamiento) => {
+//     return new Promise ((resolve, reject) => {
+//         if (comportamiento) {
+//             const telefono = {
+//                 modelo: `Iphone 12`,
+//                 capacidad: `128Gb`,
+//                 color: `rojo`
+//             }
+//             resolve(telefono);
+//         } else {
+//             reject(`Te has portado mal`);
+//         }
+//     });
+// };
+
+// const chequeaCalificacion = (calificacion) => {
+//     return new Promise ((resolve, reject) => {
+//         if (calificacion > 85) {
+//             resolve(`Buen trabajo`);
+//         }else {
+//             reject(`Tienes que estudiar mejor`);
+//         }
+//     });
+// };
+
+// const mePorteBien = chequeaComportamiento(true);
+// const estudieBien = chequeaCalificacion(89);
+
+// const promesas = [mePorteBien, estudieBien];
+
+// Promise.all(promesas)
+//     .then((values) => {
+//         console.log(values)
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+
+//async await --> sirve para trabajar más facil con promises
+
+const chequeaComportamiento = (comportamiento) => {
+    return new Promise ((resolve, reject) => {
+        if (comportamiento) {
+            const telefono = {
+                modelo: `Iphone 12`,
+                capacidad: `128Gb`,
+                color: `rojo`
+            }
+            resolve(telefono);
+        } else {
+            reject(`Te has portado mal`);
+        }
+    });
+};
+
+const chequeaCalificacion = (calificacion) => {
+    return new Promise ((resolve, reject) => {
+        if (calificacion > 85) {
+            resolve(`Buen trabajo`);
+        }else {
+            reject(`Tienes que estudiar mejor`);
+        }
+    });
+};
+
+const pideTelefonoNuevo = async (comportamiento, calificacion) => {
+    try {
+        const promesas = await Promise.all([chequeaComportamiento(comportamiento), chequeaCalificacion(calificacion)]);
+        console.log(promesas);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+pideTelefonoNuevo(true, 100);
--- practica/practica2.js
+++ practica/practica2.js
@@ -7,7 +7,7 @@
 // const api = new XMLHttpRequest();
 // api.open('GET', url, true);
 // api.onreadystatechange = function() {
-//     if( this.status == 200 && this.readyState == 4) {
+//     if( this.status === 200 && this.readyState === 4) {
 //         console.log(this.responseText);
 //     }
 // };
@@ -59,31 +59,31 @@
 
 //Intento de ponerlo en funciones para utilizar los callbacks
 
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-let url_api = `https://rickandmortyapi.com/api/character/`;
+// let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+// let url_api = `https://rickandmortyapi.com/api/character/`;
 
-const funcion = (url, callback) => {
-   const api = new XMLHttpRequest();
-   api.open('GET', url, true);
-   api.onreadystatechange = function() {
-       if( this.status == 200 && this.readyState == 4) {
-           let datos = JSON.parse(this.responseText);
-           let i = 0;
-           for (let x of datos.results) {
-               i++;
-               console.log(x.id);
-               console.log(x.name);
-               console.log(x.species);
+// const funcion = (url, callback) => {
+//    const api = new XMLHttpRequest();
+//    api.open('GET', url, true);
+//    api.onreadystatechange = function() {
+//        if( this.status == 200 && this.readyState == 4) {
+//            let datos = JSON.parse(this.responseText);
+//            let i = 0;
+//            for (let x of datos.results) {
+//                i++;
+//                console.log(x.id);
+//                console.log(x.name);
+//                console.log(x.species);
 
-               if (i >= 10) {
-                   break
-               }
-           }
-       }
-   };
-   api.send();
-};
+//                if (i >= 10) {
+//                    break
+//                }
+//            }
+//        }
+//    };
+//    api.send();
+// };
 
 
-funcion(url_api);
+// funcion(url_api);
 
