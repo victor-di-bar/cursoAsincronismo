a2dd94b39abc154caf7814a1e1624211b218046b
victor-di-bar <victor_damm_8@hotmail.com>
2022-08-02 19:50:52 +0200

    Añado los callbacks e intento de promises con API

--- practica/practica2.js
+++ practica/practica2.js
@@ -57,7 +57,7 @@
 
 
 
-//Intento de ponerlo en funciones para utilizar los callbacks
+//Ponerlo en funciones para utilizar los callbacks
 
 // let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 // let url_api = `https://rickandmortyapi.com/api/character/`;
@@ -68,22 +68,54 @@
 //    api.onreadystatechange = function() {
 //        if( this.status == 200 && this.readyState == 4) {
 //            let datos = JSON.parse(this.responseText);
-//            let i = 0;
-//            for (let x of datos.results) {
-//                i++;
-//                console.log(x.id);
-//                console.log(x.name);
-//                console.log(x.species);
-
-//                if (i >= 10) {
-//                    break
-//                }
+//                console.log(datos.results[0].id);
+//                setTimeout(() => {
+//                 callback();
+//                 },3000);
+//                console.log(datos.results[0].name);
+//                console.log(datos.results[0].species);
 //            }
 //        }
+//     api.send();
 //    };
-//    api.send();
+   
+// const funcion2 = () => {
+//     console.log(`Esto es el callback`);
 // };
 
 
-// funcion(url_api);
+// funcion(url_api, funcion2);
+
+
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let url_api = `https://rickandmortyapi.com/api/character/`;
+
+const funcion = new Promise ((resolve, reject) => {
+    const api = new XMLHttpRequest();
+    api.open('GET', url_api, true);
+    api.onreadystatechange = function() {
+        if( this.readyState === 4) {
+            this.status === 200
+            let datos = JSON.parse(this.responseText);
+            let respuesta = console.log(datos.results[0].name)
+            ? resolve(respuesta)
+            : reject(`Error fatal`);
+                // setTimeout(() => {
+                //  callback();
+                //  },3000);
+                // console.log(datos.results[0].name);
+                // console.log(datos.results[0].species);
+            }   
+        };
+        api.send();
+        });
+
+funcion
+    .then((value) => {
+        console.log(value);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
 
